14a2ea05335d5b605c8b0ca8eabe51a4f06d9d8c
Rayen20 <rayane.limem@gmail.com>
2022-03-16 16:33:10 +0100

    updates

--- src/services/carservice.js
+++ src/services/carservice.js
@@ -1,20 +1,23 @@
 import firebase from "/home/cartesiam/Documents/test/testapp/src/firebase.js";
 const db = firebase.ref("/cars");
 class carservice {
+
+  //list cars
   getAll() {
     return db;
   }
+  //create car
   create(car) {
     return db.push(car);
   }
+  // update car
   update(key, value) {
     return db.child(key).update(value);
   }
+  // delete car
   delete(key) {
     return db.child(key).remove();
   }
-  deleteAll() {
-    return db.remove();
-  }
+ 
 }
 export default new carservice();
