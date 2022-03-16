d25174000a56d6f72c18896445c5152e30034f11
Rayen20 <rayane.limem@gmail.com>
2022-03-16 16:37:42 +0100

    updates

--- src/services/userservice.js
+++ src/services/userservice.js
@@ -1,20 +1,23 @@
 import firebase from "/home/cartesiam/Documents/test/testapp/src/firebase.js";
 const db = firebase.ref("/users");
 class userservice {
+
+  // list users
   getAll() {
     return db;
   }
+  // create new user
   create(user) {
     return db.push(user);
   }
+  //update user
   update(key, value) {
     return db.child(key).update(value);
   }
+  // delete user
   delete(key) {
     return db.child(key).remove();
   }
-  deleteAll() {
-    return db.remove();
-  }
+ 
 }
 export default new userservice();
